83c289d53218a7934e56351502dcccbcdf7a2150
Samtrent17 <xamsamuel079@gmail.com>
2024-11-08 14:46:56 +0300

    Updated initial commit for the website

--- page.js
+++ page.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header")
 
-window.addEventListener("Scroll", function()){
-    header
-};
+window.addEventListener("Scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+    
+});
